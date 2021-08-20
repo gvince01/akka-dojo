239a37276d13444e2955cce0314d8166e81798f4
George Vincent <gvincent247@gmail.com>
2021-08-20 13:14:56 +0100

    Addition of needed imports

--- src/main/scala/ex5/Ex5Main.scala
+++ src/main/scala/ex5/Ex5Main.scala
@@ -9,15 +9,15 @@ import akka.util.ByteString
 
 import java.nio.file.{ Path, Paths }
 import scala.collection.immutable
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
 
 object Ex5Main extends App with CsvProcessor {
 
   // Create an actor system which we can use to execute our code
   implicit val system: ActorSystem = ActorSystem()
-  val inputPath: Path = Paths.get("src/main/resources/ex4/country-list-corrupted.csv")
-  val outputPath: Path = Paths.get("src/main/resources/ex4/country-list-fixed.csv")
+  val inputPath: Path = Paths.get("src/main/resources/ex5/country-list-corrupted.csv")
+  val outputPath: Path = Paths.get("src/main/resources/ex5/country-list-fixed.csv")
 
       // use this code block to get your stream to terminate
 //    .run
@@ -36,8 +36,6 @@ trait CsvProcessor {
   val inputCSVParser: Flow[ByteString, List[ByteString], NotUsed] = CsvParsing.lineScanner()
 
   val outputCSVFormatter: Flow[immutable.Iterable[String], ByteString, NotUsed] = CsvFormatting.format()
-
-  val flow: Flow[List[String], List[String], NotUsed] = ???
 
   def inputFileSource(inputPath: Path): Source[ByteString, Future[IOResult]] = {
     FileIO.fromPath(inputPath)
